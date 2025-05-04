b23282e296900f7a0e79de949c97597f980c5f5b
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-05-04 01:07:31 -0300

    add-criando uma funçao

---
+++ script/vvalida.js
@@ -0,0 +1,10 @@
+//Função
+
+document.getElementById("botao").onclick = function () {
+    const resposta = document.getElementById("minharesposta").value;
+    document.getElementById("respondendo").textContent = `${resposta}`;
+  }
+  
+
+  
+
