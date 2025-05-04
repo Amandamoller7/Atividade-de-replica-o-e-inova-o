7933c41269ae5814cd0031369e70c07e1d49fdd3
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-05-04 01:17:42 -0300

    add-tentando

--- script/vvalida.js
+++ script/vvalida.js
@@ -2,8 +2,8 @@
 
 document.getElementById("botao").onclick = function () {
     const resposta = document.getElementById("minharesposta").value;
-    document.getElementById("respondendo").textContent = `${resposta}`;
-  }
+    document.getElementById("respondendo").textContent = resposta;
+  };
   
 
   
